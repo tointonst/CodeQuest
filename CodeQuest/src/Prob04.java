9f13b03e779956197a5b20d32bd8964ccb83bc95
steven.tointon <steven.tointon@MU-C3-03.mullen.pvt.k12.co.us>
2015-04-09 09:04:27 -0600

    adding prob04

---
+++ CodeQuest/src/Prob04.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class Prob04
+	{
+	public static void prob04() throws FileNotFoundException
+		{
+		Scanner file = new Scanner( new File( "prob04.txt" ) );
+	    while(file.hasNext())
+	    	{
+	    	
+	    	}
+	    	
+		}
+
+	}
